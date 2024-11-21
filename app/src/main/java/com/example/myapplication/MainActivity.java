81722b2019adf51a7b89661065e2ee0a1e8b4b70
noa1107 <noadachbash@gmail.com>
2024-11-21 11:24:14 +0200

    move between screens

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -28,23 +30,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
-            
         });
-
 
         initUI();
     }
 
     private void initUI() {
-
         usernameInput = findViewById(R.id.username_input);
         passwordInput = findViewById(R.id.pasword_input);
-
         loginbtn = findViewById(R.id.login_btn);
 
-        loginbtn.setOnClickListener(username = usernameInput.toString(), password = passwordInput.toString(),
-        Log.i("Text Credentials", MessageFormat.format("Username : {0} and password : {1}", usernameInput.toString(), passwordInput.toString())));
+        // Set the click listener for the login button
+        loginbtn.setOnClickListener(v -> {
+            String username = usernameInput.getText().toString(); // Get username text
+            String password = passwordInput.getText().toString(); // Get password text
+            // Log the credentials for debugging
+            Log.i("Text Credentials", MessageFormat.format("Username: {0} and Password: {1}", username, password));
+
+            new View.OnClickListener(){
+                public void onClick(View V){
+                    Intent signUp = new Intent(MainActivity.this, VideoActivity.class);
+                    startActivity(signUp);
+                }
+            };
+        });
     }
 }
-
